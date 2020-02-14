cf1751ebf200b38057198d6e1051694367084d62
taodtu <taodtu@gmail.com>
2020-02-14 22:00:20 +0000

    seed usertable

--- model/seedTable.js
+++ model/seedTable.js
@@ -1,14 +1,22 @@
 const { dbClincet } = require("../config");
 const { topicData, userData } = require("../data/index");
 
-module.exports = async () =>
-  Promise.all(
-    topicData.map(topic =>
-      dbClincet
-        .put({
-          TableName: "topicsTable",
-          Item: { slug: topic.slug, description: topic.description }
-        })
-        .promise()
-    )
+module.exports = async () => {
+  const topicSeed = topicData.map(({ slug, description }) =>
+    dbClincet
+      .put({
+        TableName: "topicsTable",
+        Item: { slug, description }
+      })
+      .promise()
   );
+  const userSeed = userData.map(({ username, name, avatar_url }) =>
+    dbClincet
+      .put({
+        TableName: "usersTable",
+        Item: { username, name, avatar_url }
+      })
+      .promise()
+  );
+  return Promise.all([...topicSeed, ...userSeed]);
+};
