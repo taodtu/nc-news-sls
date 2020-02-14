ff1af409d335d9fd56cd7540d0cee37a1f10cb49
taodtu <taodtu@gmail.com>
2020-02-14 12:25:50 +0000

    create dbClient object

--- handler.js
+++ handler.js
@@ -1,16 +1,12 @@
-'use strict';
+"use strict";
+const dbClient = require("./config");
 
-module.exports.hello = async event => {
+module.exports.seed = async event => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify({
+      message: "Go Serverless v1.0! Your function executed successfully!"
+    })
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
