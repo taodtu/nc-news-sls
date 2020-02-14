b1f8c384ad43a1a0b2a6f3956816b6d13f9b8197
taodtu <taodtu@gmail.com>
2020-02-14 18:02:05 +0000

    create delete table handler

--- handler.js
+++ handler.js
@@ -3,6 +3,25 @@ const { db, dbClincet } = require("./config");
 const { topicData, userData } = require("./data/index");
 
 module.exports.seed = async event => {
+  try {
+    await Promise.all(
+      topicData.map(topic =>
+        dbClincet
+          .put({
+            TableName: "topicsTable",
+            Item: { slug: topic.slug, description: topic.description }
+          })
+          .promise()
+      )
+    );
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "internal error"
+      })
+    };
+  }
   return {
     statusCode: 200,
     body: JSON.stringify({
@@ -12,15 +31,22 @@ module.exports.seed = async event => {
 };
 module.exports.dropTable = async event => {
   const tables = ["topicsTable", "usersTable"];
+  const deleteTablePromise = table => {
+    return new Promise((resolve, reject) => {
+      db.deleteTable(
+        {
+          TableName: table
+        },
+        (err, data) => {
+          if (err) return reject(err);
+          resolve(data);
+        }
+      );
+    });
+  };
 
   try {
-    await Promise.all(
-      tables.map(table =>
-        db.deleteTable({
-          TableName: table
-        })
-      )
-    );
+    await Promise.all(tables.map(table => deleteTablePromise(table)));
   } catch (err) {
     return {
       statusCode: 500,
