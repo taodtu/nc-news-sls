d8e9d3a7c84dca245de2b6341cad903228eca339
taodtu <taodtu@gmail.com>
2020-02-19 20:24:02 +0000

    refactor seed article table

--- model/seedTable.js
+++ model/seedTable.js
@@ -15,7 +15,7 @@ module.exports = async () => {
         resolve(data);
       });
     });
-
+  /*
   const paramsTopicAndUser = {
     RequestItems: {
       NcNewsTable: [
@@ -43,53 +43,16 @@ module.exports = async () => {
     ReturnConsumedCapacity: "TOTAL"
   };
 
-  await seedTablePromise(paramsTopicAndUser);
+  await seedTablePromise(paramsTopicAndUser);*/
   // batchWriteItem can wirte max 25 request so this array is divided
-  /*  const articleInput = articleData.reduce(
+  const articleInput = articleData.reduce(
     (a, article, index) => {
-      const { topic, author, votes, created_at, ...rest } = article;
-      const item1 = {
-        ...rest,
-        sk: "topicDate",
-        data: `${topic}#${created_at}`,
-        topic,
-        author,
-        votes,
-        created_at
-      };
-      const item2 = {
-        ...rest,
-        sk: "topicVote",
-        data: `${topic}#${votes}`,
-        topic,
-        author,
-        votes,
-        created_at
-      };
-      const item3 = {
-        ...rest,
-        sk: "topicAuthor",
-        data: `${topic}#${author}`,
-        topic,
-        author,
-        votes,
-        created_at
-      };
-      const item4 = {
-        ...rest,
-        sk: "author",
-        data: author,
-        topic,
-        author,
-        votes,
-        created_at
-      };
-      a[Math.floor(((index + 1) * 4) / 22)].push(item1, item2, item3, item4);
+      a[Math.floor((index + 1) / 22)].push(article);
       return a;
     },
-    [...Array(Math.ceil((articleData.length * 4) / 22))].map(e => [])
+    [...Array(Math.ceil(articleData.length / 22))].map(e => [])
   );
-  //seed article data 
+  //seed article data
   for (let i = 0; i < articleInput.length; i++) {
     const params = {
       RequestItems: {
@@ -98,14 +61,21 @@ module.exports = async () => {
             PutRequest: {
               Item: {
                 pk: { S: article.article_id },
-                sk: { S: article.sk },
-                data: { S: article.data },
+                sk: { S: article.created_at },
                 topic: { S: article.topic },
                 author: { S: article.author },
                 votes: { S: `${article.votes}` },
                 created_at: { S: article.created_at },
                 title: { S: article.title },
-                body: { S: article.body }
+                body: { S: article.body },
+                gsi_1pk: { S: "articleVote" },
+                gsi_1sk: { S: `${article.votes}` },
+                gsi_2pk: { S: "articlesAuthorDate" },
+                gsi_2sk: { S: `${article.author}#${article.created_at}` },
+                gsi_3pk: { S: "articlesTopicDate" },
+                gsi_3sk: { S: `${article.topic}#${article.created_at}` },
+                gsi_4pk: { S: "articlesTopicVote" },
+                gsi_4sk: { S: `${article.topic}#${article.votes}` }
               }
             }
           }))
@@ -114,9 +84,9 @@ module.exports = async () => {
       ReturnConsumedCapacity: "TOTAL"
     };
     await seedTablePromise(params);
-  } */
+  }
   // batchWriteItem can wirte max 25 request so this array is divided
-  const commentInput = commentData.reduce(
+  /* const commentInput = commentData.reduce(
     (a, comment, index) => {
       a[Math.floor((index + 1) / 22)].push(comment);
       return a;
@@ -149,5 +119,5 @@ module.exports = async () => {
       ReturnConsumedCapacity: "TOTAL"
     };
     await seedTablePromise(params);
-  }
+  }*/
 };
